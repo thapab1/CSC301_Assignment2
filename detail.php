f35e003b8cd018a1a5297621605fba5fbc9ae75a
thapab1 <58610028+thapab1@users.noreply.github.com>
2020-01-27 22:51:19 -0500

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,160 @@
+<?php
+$apartments = [
+    [   
+        "household-name" => "Jessica",
+        "household-age" => "24",
+        "household-sex" => "Female",
+        "household-img" => "https://mediaslide-europe.storage.googleapis.com/milk/pictures/1735/3110/large-1562343538-1aed088015715cc7f7843fb28f861fb6.jpg",
+        "description" => "Convenient location walking distance of Oakley Square. Modern amenities, shared off-street parking, respectful roommates, and all utilities and Wi-Fi included. Please contact me on social if interested",
+        "house-img" => "https://wp.zillowstatic.com/naked-apartments/wp-content/uploads/sites/1/2018/06/large-7-07c4dc.jpg",
+        "lifestyle-cleanliness" => "Average",
+        "lifestyle-bedtime" => "11PM",
+        "lifestyle-food" => "Almost Anything",
+        "lifestyle-occupation" => "College Student",
+        "flatmate-expectation" => "Age: 25 - 37 <br> Sex: Female <br> Smoking: Outside only <br> Students Only: No",
+        "price" => "$900/month",
+        "phone-number" => "819-346-6698"
+    ],
+    [   
+        "household-name" => "Omny",
+        "household-age" => "30",
+        "household-sex" => "Male",
+        "household-img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIOlgen1CHx683xfV-hVatZcjV7j_3Sc9hg2wbM9Uf1YibE6Qr",
+        "description" => "Mature male from Colorado looking for female roommate to share 4 bed, 2 bath house, short or long term. Not looking for a relationship, I just prefer a female because my 16yr old daughter attending UC lives with me, my 14yr old daughter occasionally visits on a weekend and neither my ex nor myself want another dude around my daughters. It would be great to find another UC student but older roomies are welcome. I work in quality assurance for the aerospace industry and I'm a part-time photographer. Minimum 1-month stay.",
+        "house-img" => "http://www.home4docs.com/wp-content/uploads/2017/08/brown3.jpg",
+        "lifestyle-cleanliness" => "Sparkling clean",
+        "lifestyle-bedtime" => "1AM",
+        "lifestyle-food" => "Almost Anything",
+        "lifestyle-occupation" => "Amazon Warehouse Worker",
+        "flatmate-expectation" => "Age: 0-100 <br> Sex: Female <br> Smoking: Anywhere <br> Students Only: No",
+        "price" => "$600/month",
+        "phone-number" => "719-346-6699"
+    ],
+    [   
+        "household-name" => "Hot-Rod",
+        "household-age" => "46",
+        "household-sex" => "Male",
+        "household-img" => "https://upload.wikimedia.org/wikipedia/commons/7/75/Luke_Evans_2014_Comic_Con_%28cropped%29.jpg",
+        "description" => "Still have a room available for Spring session in an all female house. Looking for students or professionals that want to live near Univ of Cincinnati, OTR, Downtown CBD, Walnut Hills or Hyde Park. <br>The room is very spacious and furnished with a queen size bed, study desk and chair, and storage shelves and rack. <br>Electric & Gas, Water and Wi-Fi are included in the rent.",
+        "house-img" => "https://pix10.agoda.net/hotelImages/245/245371/245371_130110153425401.jpg?s=1024x768",
+        "lifestyle-cleanliness" => "Average",
+        "lifestyle-bedtime" => "12AM",
+        "lifestyle-food" => "Allergic to Seafood",
+        "lifestyle-occupation" => "Uber Driver",
+        "flatmate-expectation" => "Age: 0 - 30 <br> Sex: Male or Female <br> Smoking: Outside only <br> Students Only: No",
+        "price" => "$700/month",
+        "phone-number" => "819-346-6690"
+    ],
+    [   
+        "household-name" => "Eric",
+        "household-age" => "32",
+        "household-sex" => "Male",
+        "household-img" => "https://www.biography.com/.image/t_share/MTUxNjU1MjI3NTc2MDM0NTcy/gettyimages-689502038.jpg",
+        "description" => "My girlfriend and I live in the home with our dog. Looking for a roommate to take over a spare room with it's own en suite bathroom. The house is in walking distance to the new brewery and shops that are being built in Madisonville on Madison rd. 10-15 minute drive to Clifton or downtown. Looking for someone that is hospitable and that hopefully shares some of our same interests.",
+        "house-img" => "https://media.estatesit.uk/data/METROPOLITANCROWN/PHOTOS/metro-000509-p-w-4k91350sr.jpg",
+        "lifestyle-cleanliness" => "Sparlking Clean",
+        "lifestyle-bedtime" => "11PM",
+        "lifestyle-food" => "Almost Anything",
+        "lifestyle-occupation" => "Teacher",
+        "flatmate-expectation" => "Age: 25 - 40 <br> Sex: Female <br> Smoking: Outside only <br> Students Only: Yes",
+        "price" => "$600/month",
+        "phone-number" => "819-346-6691"
+    ],
+    [   
+        "household-name" => "Lily",
+        "household-age" => "22",
+        "household-sex" => "Female",
+        "household-img" => "http://images2.fanpop.com/images/photos/6400000/Himym-how-i-met-your-mother-6418558-1707-2560.jpg",
+        "description" => "Aspen Pines Apartments Wilder, KY. Right outside the Newport line, 15 minute from downtown Cincinnati, 10 minutes from Northern Kentucky University. Third floor views of downtown Cincinnati.",
+        "house-img" => "https://photos.spareroom.com/images/flatshare/listings/large/20/32/203226640.jpg",
+        "lifestyle-cleanliness" => "Great",
+        "lifestyle-bedtime" => "1AM",
+        "lifestyle-food" => "Allergic to Seafood",
+        "lifestyle-occupation" => "College Student",
+        "flatmate-expectation" => "Age: 22 - 30 <br> Sex: Male or Female <br> Smoking: No smoking <br> Students Only: No",
+        "price" => "$480/month",
+        "phone-number" => "819-346-6693"
+    ],
+    [   
+        "household-name" => "Danny",
+        "household-age" => "29",
+        "household-sex" => "Male",
+        "household-img" => "https://pmctvline2.files.wordpress.com/2017/12/danny-masterson.png?w=616",
+        "description" => "A whole flat in OTR. Have your own kitchen and bathroom. Perfect location on Main Street next to The Pony and MOTR. Rent included all utilities, parking, and laundry.",
+        "house-img" => "https://westernpleasureranch.com/wp-content/uploads/2015/03/WesternPleasureGuestRanch_2065.jpg",
+        "lifestyle-cleanliness" => "Poor",
+        "lifestyle-bedtime" => "11PM",
+        "lifestyle-food" => "Almost Anything",
+        "lifestyle-occupation" => "Rancher",
+        "flatmate-expectation" => "Age: 23 - 32 <br> Sex: Male or Female <br> Smoking: Outside only <br> Students Only: No",
+        "price" => "$1,000/month",
+        "phone-number" => "819-346-6695"
+    ]
+];
+
+if(!isset($_GET['id'])){
+	echo 'Please visit our <a href="index.php">Home Page</a>.';
+	die();
+}
+if($_GET['id']<0 || $_GET['id']>count($apartments)-1){
+	echo 'Something went wrong! Please come back to our <a href="index.php">Home Page</a>.';
+	die();
+} else {
+    $id = $_GET['id'];
+}
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <title>RoomsForRent</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-9">
+                    <h4>Description:</h4>
+                    <?= $apartments[$id]['description']?>
+                    <br><br>
+                    <div class="row">
+                        <div class="col-5">
+                            <h4>About household:</h4>
+                            <ul>
+                                <li>Cleanliness: <?= $apartments[$id]['lifestyle-cleanliness']?></li>
+                                <li>Food: <?= $apartments[$id]['lifestyle-food']?></li>
+                                <li>Bedtime: <?= $apartments[$id]['lifestyle-bedtime']?></li>
+                                <li>Price asking: <?= $apartments[$id]['price']?></li>
+                            </ul>
+                        </div>
+                        <div class="col-7">
+                            <img src="<?=$apartments[$id]['house-img']?>" alt="room-<?= $id ?>'" class="img-responsive" style="max-height: 400px; max-width: 400px;">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <img src="<?= $apartments[$id]['household-img'] ?>" alt="houshold-<?= $id ?>" class="img-responsive rounded-circle" style="max-height: 100px; max-width: 100px;">
+                    <strong><?= $apartments[$id]['household-name'] ?></strong>
+                    <br>
+                    <small><?= $apartments[$id]['lifestyle-occupation'] ?></small>
+                    <br>
+                    <small><?= $apartments[$id]['household-sex'].', '.$apartments[$id]['household-age']. ' years old' ?></small> 
+                    <hr class="small-margin-top">
+                    <p>Phone number: <?= $apartments[$id]['phone-number'] ?></p>    
+                </div>
+            </div>
+        </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
